2a42a55807e4b36998a7b6ba46a9b0819223b38a
= <you@example.com>
2024-10-21 20:11:50 +0800

    Site updated: 2024-10-21 20:11:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/12/RyoTech的自白书——我为什么想建立这个博客网站？/","2024/08/12/【硬件科普】中央处理器（CPU）/","2024/10/21/【计维】PE系统简介/"];function toRandomPost(){
+var posts=["2024/08/12/【硬件科普】中央处理器（CPU）/","2024/08/12/RyoTech的自白书——我为什么想建立这个博客网站？/","2024/10/21/【计维】PE系统简介/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
